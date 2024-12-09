827492734219c441f5093b03a0fc765bfd878745
Alexandr <Alexandr@mail.com>
2024-12-09 15:58:48 +0300

    вариант проверки подсписка в списке count()

--- old_cipher.py
+++ old_cipher.py
@@ -2,11 +2,23 @@
 # "Слишком древний шифр"
 
 number_list = [ i + 3 for i in range(18)]
-fist_list = [ i for i in range(21) if i > 0]
-parol = []
+first_list = [ i for i in range(21) if i > 0]
+alt_first_list = first_list[:]
+
+for row in number_list:
+    parol = f'{row} '
+    for i in first_list:
+        for j in alt_first_list:
+            element = i + j
+            if row  % element == 0:
+                result_ctr = parol.count(f'{j}{i}')
+                if result_ctr > 0:
+                    continue
+                else:
+                    parol += f'{i}{j}'
+    print(parol)
 
 
-print(number_list)
-print(fist_list)
+
 
 
