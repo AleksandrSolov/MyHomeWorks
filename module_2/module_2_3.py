a21b89d1b8b4fc98a4ee4d03330dacf1805359f3
Alexandr <Alexandr@mail.com>
2024-12-24 18:39:19 +0300

    исправлено

--- module_2/module_2_3.py
+++ module_2/module_2_3.py
@@ -1,6 +1,6 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 322, 13, 0, 99, 5, 9, 8, 7, 6, 5]
 n_index = 0
-while n_index <= len(my_list):
+while n_index < len(my_list):
      if my_list[n_index] > 0:
           print(my_list[n_index])
      elif my_list[n_index] < 0:
