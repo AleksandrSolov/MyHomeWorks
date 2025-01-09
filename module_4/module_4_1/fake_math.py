5fd5b5d81f7d4f7445fc991a58102cf01e8c9746
Alexandr <Alexandr@mail.com>
2025-01-09 23:07:55 +0300

    09.01.2025

--- module_4/module_4_1/fake_math.py
+++ module_4/module_4_1/fake_math.py
@@ -1,1 +1,9 @@
+def divide(first, second):
+    if second != 0:
+        namba = first / second
+        return namba
+    else:
+        namba = 'Ошибка'
+        return namba
+
 
---
+++ module_4/module_4_1/true_math.py
@@ -0,0 +1,11 @@
+import math
+def divide(first, second):
+    if second != 0:
+        resalt = first / second
+        return resalt
+    else:
+        from math import inf
+        resalt = math.inf
+        return resalt
+        #print(float("inf"))
+
